ed446b456d69483a2a3de286e131bee3ff981b48
lagovi <61742495+lagovi@users.noreply.github.com>
2026-05-01 01:42:10 +0300

    Create html_sig.c

---
+++ html_sig.c
@@ -0,0 +1,45 @@
+#include <glib.h>
+#include "common/claws.h"
+#include "common/version.h"
+#include "plugin.h"
+
+gint plugin_init(gchar **error)
+{
+    g_print("HtmlSigPlugin: Initialized successfully.\n");
+    return 0;
+}
+
+gboolean plugin_done(void)
+{
+    g_print("HtmlSigPlugin: Unloaded successfully.\n");
+    return TRUE;
+}
+
+const gchar *plugin_name(void)
+{
+    return "HTML Signature Injector";
+}
+
+const gchar *plugin_desc(void)
+{
+    return "PoC Plugin: Detects HTML signatures and converts text/plain to multipart/alternative on the fly.";
+}
+
+const gchar *plugin_type(void)
+{
+    return "GTK3";
+}
+
+const gchar *plugin_licence(void)
+{
+    return "GPL";
+}
+
+struct PluginFeature *plugin_provides(void)
+{
+    static struct PluginFeature features[] = {
+        {PLUGIN_UTILITY, "HTML Signature"},
+        {PLUGIN_NOTHING, NULL}
+    };
+    return features;
+}
